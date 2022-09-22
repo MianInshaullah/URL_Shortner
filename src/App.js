4b7740c9f659c7e6c640b49ab0d92a498539e0a3
Mian Inshaullah <73126487+MianInshaullah@users.noreply.github.com>
2022-09-23 04:32:08 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState } from "react";
+import BackgroundAnimate from './BackgroundAnimate';
+import InputShortener from './InputShortener';
+import LinkResult from './LinkResult';
+
+function App() {
+  
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="container">
+      <InputShortener setInputValue={setInputValue}/>
+      <BackgroundAnimate/>
+      <LinkResult inputValue ={inputValue}/>
+    </div>
+  );
+}
+
+export default App;
